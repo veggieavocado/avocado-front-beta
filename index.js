4cd0148e8ecbce6cc0a15fceb824efeff7e3bec8
1 <ppark9553@gmail.com>
2018-08-08 17:02:22 +0900

    폴더 모두 정리

--- index.js
+++ index.js
@@ -25,78 +25,47 @@ console.log(`서버가 http://${HOST}:${PORT} 에서 작동하고 있습니다.`
 // 두 번째 인자는 디폴트로 (req, res)라고 두고 텍스트를 보여주고 싶으면, res.send를
 // html을 보내고 싶으면, res.render함수를 사용합니다.
 
-// 테스트 페이지: https://www.veggieavocado.com/
+// 홈 페이지: https://www.veggieavocado.com/
 app.get('/', (req, res) => {
-  res.send('제대로 작동합니다!');
-});
-
-// 홈 페이지: https://www.veggieavocado.com/home/
-app.get('/home', (req, res) => {
-  res.render('home.html');
+  res.render('production/home.html');
 });
 
 // 로그인 페이지: https://www.veggieavocado.com/login/
 app.get('/login', (req, res) => {
-  res.render('login.html');
+  res.render('production/login.html');
 });
 
 // 회원가입 페이지: https://www.veggieavocado.com/register/
 app.get('/register', (req, res) => {
-  res.render('register.html');
-});
-
-// 템플릿뷰 페이지:  https://www.veggieavocado.com/template
-app.get('/template', (req, res) => {
-  res.render('templateview.html');
-});
-
-// 템플릿뷰 페이지:  https://www.veggieavocado.com/maketemplate
-// 템플릿에 데이터 보여주기 테스트용
-app.get('/maketemplate', (req, res) => {
-  res.render('maketemplate.html');
+  res.render('production/register.html');
 });
 
 // ppt선택 페이지: https://www.veggieavocado.com/pptselect
 app.get('/pptselect', (req, res) => {
-  res.render('pptselect.html');
+  res.render('production/pptselect.html');
 });
 
-// 이메일선택 페이지: https://www.veggieavocado.com/emailselect
-app.get('/emailselect', (req, res) => {
-  res.render('emailselect.html');
+// 템플릿뷰 페이지:  https://www.veggieavocado.com/template
+app.get('/template', (req, res) => {
+  res.render('production/templateview.html');
 });
 
-//유저가 템플릿 관리하는 페이지: https://www.veggieavocado.com/mydoc
-app.get('/mydoc', (req, res) => {
-  res.render('mydoc.html')
-});
-
-// 소개 페이지: https://www.veggieavocado.com/introduction
-app.get('/introduction', (req, res) => {
-  res.render('introduction.html');
+// 소개 페이지: https://www.veggieavocado.com/about
+app.get('/about', (req, res) => {
+  res.render('production/about.html');
 });
 
 // 문의 페이지: https://www.veggieavocado.com/customer
 app.get('/customer', (req, res) => {
-  res.render('customer.html');
+  res.render('production/customer.html');
 });
 
-// 관리자 데브 페이지: https://www.veggieavocado.com/admin
-app.get('/admin', (req, res) => {
-  res.render('admin2.html');
-});
-
-// 관리자 옵스 페이지: https://www.veggieavocado.com/devops
-app.get('/devops', (req, res) => {
-  res.render('devops.html');
-});
-
-// 원티드 컨텐츠 페이지: https://www.veggieavocado.com/wanted
+// 원티드 컨텐츠 페이지: https://www.veggieavocado.com/contents/wanted
 app.get('/wanted', (req, res) => {
-  res.render('wanted.html');
+  res.render('production/wanted.html');
 });
 
 // 임시 : https://www.veggieavocado.com/navbar
 app.get('/navbar', (req, res) => {
-  res.render('navbar.html');
+  res.render('production/navbar.html');
 });
